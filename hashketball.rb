d5ca29e2194413363749353d09d92b3cc82b123e
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 20:14:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,7 @@ def team_colors(team_name)
       
       binding.pry
       if value.include?(team_name)
-        color = access[home_away][team_info_selector][value]
+        color = access[home_away][:colors][value]
       end
     end
   end
